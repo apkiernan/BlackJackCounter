0fddffc0e4bfd43ac4cc8ef0178e73418a19f7e7
Andrew Kiernan <apkiernan@gmail.com>
2014-01-15 20:43:29 -0500

    fixed issue with dealer's count not adding correctly.

--- js/blackjack.js
+++ js/blackjack.js
@@ -3,6 +3,7 @@ var shoe = []
 , dealerHand = []
 , playerCount
 , dealerCount;
+
 function getShoe(){
   //Add desired number of decks to the shoe
   var deck = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11]
@@ -28,6 +29,7 @@ function shuffle(){
     }
   return shoe;
 }
+
 function deal(){
   playerHand = [];
   dealerHand = [];
@@ -40,14 +42,16 @@ function deal(){
   $("#dealerHand .cardArea").text(dealerCount);
   $("#bust","#playerLose","#playerWin").css({"display": "none"})
 }
+
 function hit(){
   playerHand.push(shoe.shift());
-  playerCount = sum(playerHand);
+  playerCount += playerHand[playerHand.length - 1];
   if(playerCount > 21){
     $("#bust").css({"display": "block"});
   }
   $("#playerHand .cardArea").text(playerCount);
 }
+
 function sum(x){
   var y, z = 0;
   for (var i = 0; i<x.length; i++){
@@ -56,10 +60,12 @@ function sum(x){
   }
   return z;
 }
+
 function dealerTurn(){
   dealerHand.push(shoe.shift());
-  dealerCount = sum(dealerHand);
-  while (dealerCount < 17){
+  dealerCount += dealerHand[dealerHand.length - 1];
+  $("#dealerHand .cardArea").text(dealerCount);
+  /*while (dealerCount < 17){
     if (dealerCount > 21){
       $("#playerWin").css({"display": "block"});
     } else if(dealerCount < 17){
@@ -68,12 +74,12 @@ function dealerTurn(){
     } else if(dealerCount => 17 && dealerCount <= 21){
       compareScore();
     }
-  }
+  }*/
 }
-function compareScore(playerCount, dealerCount){
+/*function compareScore(playerCount, dealerCount){
   if(playerCount > dealerCount){
     $("#playerWin").css({"display": "block"});
   } else {
     $("#playerLose").css({"display": "block"})
   }
-}
+}*/
